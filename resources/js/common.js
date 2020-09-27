05c70f3f6799fe9a96c74e12bbdd8644b97d5a9d
이노디스 이종원 <moguri61@innodis.co.kr>
2020-09-27 20:15:22 +0900

    상세페이지 작업 수정2

---
+++ resources/js/common.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    // 스크롤 바로이동
+   $(window).scroll(function () {
+		if ($(this).scrollTop() > 200) {
+			$('#quickMenu').fadeIn();
+		} else {
+			$('#quickMenu').fadeOut();
+		}
+	});
+
+    // scroll body to 0px on click
+	$('#btnTop').bind("click", function(e) {
+		// Prevents the default action to be triggered.
+		e.preventDefault();
+
+		$('body,html').animate({
+			scrollTop: 0
+		}, 500);
+	});
+
+    // 스크롤 바로이동
+    $(".anchor").click(function(e){
+        // Prevents the default action to be triggered.
+        e.preventDefault();
+
+        $('html,body').animate({
+            scrollTop : $(this.hash).offset().top-120
+        }, 500);
+    });
+});
+
+var player = videojs('daysVideo', {
+    autoplay: 'false',
+});
